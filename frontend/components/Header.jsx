2a11788b38ccc45a69a989be0fba7a35158dc1ea
Amasha Sinhalagoda <amashasinhalagoda@Amashas-MacBook-Air-2.local>
2025-03-24 20:49:26 +0530

    change styles

--- frontend/components/Header.jsx
+++ frontend/components/Header.jsx
@@ -2,13 +2,15 @@ import React from 'react'
 import Navbar from './Navbar'
 import Carousel from './Carousel'
 import About from '../pages/About'
+import Footer from '../components/Footer'
 
 const Header = () => {
   return (
-    <div className='min-h-screen mb-4 bg-cover bg-center w-full overflow-hidden bg-black'>
+    <div className='min-h-screen  bg-cover bg-center w-full overflow-hidden bg-black'>
         <Navbar/>
         <Carousel/>
         <About/>
+        <Footer/>
     </div>
   )
 }
