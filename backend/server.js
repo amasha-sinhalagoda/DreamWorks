41e9bc72e7293dc037b0d21715a5b3807cc7d661
Amasha Sinhalagoda <amashasinhalagoda@Amashas-MacBook-Air-2.local>
2025-03-24 20:35:43 +0530

    import portfolio router

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,8 @@ import express from 'express'
 import cors from 'cors'
 import dotenv from 'dotenv'
 import connectDB from './config/database.js';
+import portfolioRouter from './routes/portfolio.route.js';
+
 
 dotenv.config();
 
@@ -15,10 +17,9 @@ app.use(express.json())
 app.use(cors())
 
 //api endpoints
-app.get('/',(req,res) => {
-    res.send("API Working")
-})
+app.use('/api/portfolio', portfolioRouter);
 
 app.listen(port, () => {
     console.log(`Server is up and running on port : ${port}`);
 })
+
