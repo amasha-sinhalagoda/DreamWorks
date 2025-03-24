3bae3f7628169cb6f02f59e78360fe0c452301da
Amasha Sinhalagoda <amashasinhalagoda@Amashas-MacBook-Air-2.local>
2025-03-24 20:55:01 +0530

    add dependencies

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,25 +1,36 @@
 import React from 'react'
 
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { BrowserRouter, Route, Routes  } from 'react-router-dom'
 import Header from '../components/Header'
 import Contact from '../pages/Contact'
-import ScrollUp from '../components/ScrollUp'
 import Footer from '../components/Footer'
+import ScrollUp from '../components/ScrollUp'
+import CategoryDetail from '../pages/CategoryDetail'
+import Portfolio from '../pages/Portfolio'
+import AdminDashboard from '../pages/AdminDashboard'
+import UpdatePortfolio from '../pages/UpdatePortfolio'
+import { ToastContainer } from 'react-toastify';
 
 const App = () => {
   return (
     <BrowserRouter>
     <div className='w-full overflow-hidden'>
     <Routes>
+    
       <Route path='/' element={<Header/>}/>
       <Route path='/contact' element={<Contact />}/>
+      <Route path='/gallery' element={<Portfolio />}/>
+      <Route path= '/dashboard' element={<AdminDashboard/>}/>
+      <Route path="/portfolio/:id" element={<CategoryDetail />} />
+      <Route path='/viewGallery' element={<UpdatePortfolio/>}/>
     </Routes>
-    <ScrollUp/>
-    <Footer/>
+    
+     <ScrollUp/>
+     <ToastContainer/>
     </div>
     </BrowserRouter>
     
   )
 }
 
-export default App
+export default App;
